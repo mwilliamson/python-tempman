231bcde3b237dec1c9783383bc7e425d9be94806
Michael Williamson <mike@zwobble.org>
2013-11-16 14:11:27 +0000

    Add timeout argument to root()

--- tempman/__init__.py
+++ tempman/__init__.py
@@ -1,5 +1,7 @@
 import tempfile
 import shutil
+import time
+import os
 
 
 def create_temp_dir(dir=None):
@@ -21,13 +23,32 @@ class TemporaryDirectory(object):
         self.close()
 
 
-def root(dir):
-    return Root(path=dir)
+def root(dir, timeout=None):
+    return Root(path=dir, timeout=timeout)
     
     
 class Root(object):
-    def __init__(self, path):
+    def __init__(self, path, timeout):
         self._path = path
+        self._timeout = timeout
         
     def create_temp_dir(self):
+        self._cleanup()
+        
         return create_temp_dir(dir=self._path)
+
+    def _cleanup(self):
+        if self._timeout is not None:
+            self._delete_directories_older_than_timeout()
+            
+    def _delete_directories_older_than_timeout(self):
+        now = time.time()
+        limit = now - self._timeout
+
+        names = os.listdir(self._path)
+        for name in names:
+            path = os.path.join(self._path, name)
+            stat = os.stat(path)
+            
+            if max(stat.st_atime, stat.st_mtime) < limit:
+                shutil.rmtree(path)
--- tests/tempman_tests.py
+++ tests/tempman_tests.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from nose.tools import istest, assert_equal
 
@@ -39,3 +40,23 @@ def using_root_to_create_temporary_directories_creates_directories_under_root_pa
         root = tempman.root(parent_directory.path)
         with root.create_temp_dir() as directory:
             assert_equal([os.path.basename(directory.path)], os.listdir(parent_directory.path))
+
+
+@istest
+def directories_older_than_timeout_are_deleted_when_creating_temp_dir_under_root():
+    with tempman.create_temp_dir() as parent_directory:
+        root = tempman.root(parent_directory.path, timeout=60)
+        now = time.time()
+        
+        old_dir = os.path.join(parent_directory.path, "one")
+        os.mkdir(old_dir)
+        os.utime(old_dir, (now - 70, now - 70))
+        
+        newer_dir = os.path.join(parent_directory.path, "two")
+        os.mkdir(newer_dir)
+        os.utime(newer_dir, (now - 10, now - 10))
+        
+        with root.create_temp_dir() as directory:
+            pass
+            
+        assert_equal(["two"], os.listdir(parent_directory.path))
